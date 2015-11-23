d731ed17667050b7743857ea346db9ad294d8db9
Fabian Bützow <fabian@artcom.de>
2015-11-23 17:45:40 +0100

    Prompt user for confirmation

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 /* @flow */
 
+import _ from "lodash"
+import bluebird from "bluebird"
+import read from "read"
+import yn from "yn"
+
 import {adb, log} from "./setup"
 import * as hockeyApp from "./hockeyApp/hockeyApp"
 import {createAllActionsForDevices, filterDeployableActions} from "./actions/actionCreator"
 import {printActionsByDevice} from "./printer"
 
+const readAsync = bluebird.promisify(read)
+
 Promise.all([adb.listDevices(), hockeyApp.getAppConfigs()])
   .then(createAllActionsForDevices)
   .then(printActionsByDevice)
   .then(filterDeployableActions)
+  .then(informUser)
   .then((deployableActions) => {
     const deploy = deployableActions.map((action) => {
       return action.deploy()
@@ -18,3 +26,21 @@ Promise.all([adb.listDevices(), hockeyApp.getAppConfigs()])
   .catch((error) => {
     log.error({error}, "Error")
   })
+
+function informUser(deployableActions) {
+  if (_.isEmpty(deployableActions)) {
+    console.log("All Apps up-to-date")
+    process.exit()
+  } else {
+    return promptUser(deployableActions)
+  }
+}
+
+function promptUser(deployableActions) {
+  return readAsync({ prompt: "apply changes (y/N)?" })
+    .then((response) => {
+      if (yn(response)) {
+        return deployableActions
+      }
+    })
+}
