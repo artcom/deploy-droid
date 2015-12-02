954f11aeebcbfb5c6055bcfdff857f208571cc82
Fabian Bützow <fabian@artcom.de>
2015-12-02 16:24:52 +0100

    Reduce lines of code

--- src/hockeyApp/appConfigPrinter.js
+++ src/hockeyApp/appConfigPrinter.js
@@ -2,8 +2,10 @@
 
 import colors from "colors/safe"
 import table from "text-table"
-import type {AppConfig} from "./types"
+
 import {releaseType} from "./../setup"
+
+import type {AppConfig} from "./types"
 
 export function printAppConfigs(appConfigs: Array<AppConfig>): Array<AppConfig> {
   const header  =
@@ -12,9 +14,8 @@ export function printAppConfigs(appConfigs: Array<AppConfig>): Array<AppConfig> 
   const printableRows = appConfigs.map((appConfig) => {
     return createPrintableRow(appConfig)
   })
-  const formattedAppConfigs = table(printableRows)
 
-  console.log(header + "\n" + formattedAppConfigs + "\n")
+  console.log(header + "\n" + table(printableRows) + "\n")
   return appConfigs
 }
 
